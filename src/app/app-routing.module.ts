aec5b49275f5788bba9caa87496bf91a5426a75e
Pete Russell <pinkbatz@gmail.com>
2022-04-07 15:44:23 +1200

    Add routing, assessment and results components (placeholders), favicon, fonts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AssessmentComponent } from "src/app/assessment/assessment.component";
+import { NotFoundComponent } from "src/app/not-found/not-found.component";
+import { ResultsComponent } from "src/app/results/results.component";
+
+const routes: Routes = [
+  { path: 'assessment', component: AssessmentComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: '', redirectTo: '/assessment', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssessmentComponent } from './assessment/assessment.component';
+import { ResultsComponent } from './results/results.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssessmentComponent,
+    ResultsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
