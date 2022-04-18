1890296c6ad18db87b248013049d54cc962875da
Pete Russell <pinkbatz@gmail.com>
2022-04-18 14:40:21 +1200

    Styling improvements.

--- src/features/landingPage/routes/LandingPage.tsx
+++ src/features/landingPage/routes/LandingPage.tsx
@@ -2,10 +2,10 @@ import { Layout } from "../components/Layout";
 
 export const LandingPage = () => {
   return (
-    <Layout title="Improve your climbing" buttonText="Take assessment">
+    <Layout title="Improve your climbing" buttonText="Start assessment">
       <p>
         This website will guide you through a self-assessment of your current
-        climbing skillset to help identify strengths, and areas for
+        climbing abilities to help identify strengths and areas for
         improvement.
       </p>
 
