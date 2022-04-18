4acdbd748b52633e40321991a690a0a328f2872b
Pete Russell <pinkbatz@gmail.com>
2022-04-18 14:51:02 +1200

    Add TFC book link and add global <a> styling.

--- src/features/landingPage/routes/LandingPage.tsx
+++ src/features/landingPage/routes/LandingPage.tsx
@@ -10,7 +10,12 @@ export const LandingPage = () => {
       </p>
 
       <p>
-        The assessment is 30 questions and takes approximately 10 minutes.
+        This assessment is based on the book&nbsp;
+        <a
+          href="https://trainingforclimbing.com/buy-books/training-for-climbing-3rd-edition-2016/"
+          target="_blank"
+          rel="noreferrer">Training for Climbing</a> by Eric Hörst.
+          It's 30 questions and takes approximately 10 minutes.
       </p>
     </Layout>
   )
